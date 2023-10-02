70be1c35a7f8b554b8620c35d3a21b1e80f8cde9
shivam tiwari <shivamt2023@gmail.com>
2023-10-02 19:36:46 +0530

    add JWT Token and Bcrypt.js

--- Server/Routes/NewUser.js
+++ Server/Routes/NewUser.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const User = require("../Models/User");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const jwtSecret = "wasdevelopedagainstdraftietfoaut";
+
+// Middleware for handling validation errors
+function handleValidationErrors(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success: false, errors: errors.array() });
+  }
+  next();
+}
+
 router.post(
   "/newuser",
   [
@@ -9,22 +22,25 @@ router.post(
     body("name").isLength({ min: 5 }).withMessage("Name must be at least 5 characters"),
     body("password").isLength({ min: 5 }).withMessage("Password must be at least 5 characters"),
   ],
+  handleValidationErrors,
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
     try {
-      User.create({
+      const salt = await bcrypt.genSalt(10);
+      let securepass = await bcrypt.hash(req.body.password, salt);
+
+      const user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: securepass,
         email: req.body.email,
         location: req.body.location,
       });
-      res.json({ success: true });
+
+      // Return the created user (with sensitive info stripped)
+      const { password, ...userWithoutPassword } = user.toObject();
+      res.status(201).json({ success: true, user: userWithoutPassword });
     } catch (error) {
       console.error(error);
-      res.json({ success: false, error: "An error occurred" });
+      res.status(500).json({ success: false, error: "An error occurred" });
     }
   }
 );
@@ -37,36 +53,41 @@ router.post(
     body("email").isEmail().withMessage("Invalid email format"),
     body("password").isLength({ min: 5 }).withMessage("Incorrect password"),
   ],
+  handleValidationErrors,
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { email, password } = req.body;
-
     try {
+      const { email, password } = req.body;
       const userData = await User.findOne({ email });
 
       if (!userData) {
-        return res
-          .status(400)
-          .json({ errors: "Try Login With Correct Credentials" });
+        return res.status(400).json({ success: false, errors: "Try Login With Correct Credentials" });
       }
 
-      if (password !== userData.password) {
-        return res
-          .status(400)
-          .json({ errors: "Try Login With Correct Credentials" });
-      } else {
-        return res.json({ success: true, message: "Login successful", user: userData });
+      const passwordcompare = await bcrypt.compare(password, userData.password);
+
+      if (!passwordcompare) {
+        return res.status(400).json({ success: false, errors: "Try Login With Correct Credentials" });
       }
+
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+
+      res.json({
+        success: true,
+        message: "Login successful",
+        user: userData,
+        authToken: authToken,
+      });
     } catch (error) {
       console.error(error);
-      res.json({ success: false, error: "An error occurred" });
+      res.status(500).json({ success: false, error: "An error occurred" });
     }
   }
 );
-
 
 module.exports = router;
--- food-delivery/src/Screens/Login.jsx
+++ food-delivery/src/Screens/Login.jsx
@@ -39,6 +39,8 @@ function Login() {
       alert("Enter Valid Credentials");
     }
     if (json.success) {
+      localStorage.setItem("authToken",json.authToken)
+      console.log(localStorage.getItem("authToken"))
       navigate("/")
     }
     
